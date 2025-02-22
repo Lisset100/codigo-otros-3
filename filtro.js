28b972ef4b4054ebe0fe723ca412df5c9d290a72
Lisset Sánchez <131899300+Lisset100@users.noreply.github.com>
2025-02-21 22:53:11 -0600

    Se arregló el código

--- filtro.js
+++ filtro.js
@@ -1,63 +1,57 @@
 // Tenemos un li de productos
 
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  { nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg" },
+  { nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg" },
+  { nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg" },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg" },
+  { nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg" }
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const lista = document.getElementById("lista-de-productos")//Le cambié el name por id y le quité la s a elements
+const filtro = document.querySelector('input');// el punto es para las clases e input es un tag
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+const displayProductos = (productos) => { // se invocaba la función pero no existía así que la cree y reutilicé el código en la parte de abajo
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild); // Limpia la lista antes de agregar los productos
+  }
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+  productos.forEach(producto => {
+    let divProducto = document.createElement("div")
+    divProducto.classList.add("producto")
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+    let titulo = document.createElement("p")//cambié el nombre de la variable para que fuera más entendible y el tipo de var a let
+    titulo.classList.add("titulo")
+    titulo.textContent = producto.nombre
 
-  li.appendChild(d)
-}
+    let imagen = document.createElement("img"); // cambié el var por let
+    imagen.setAttribute('src', producto.img);
+
+    divProducto.appendChild(titulo)
+    divProducto.appendChild(imagen)
+
+    lista.appendChild(divProducto)
+  });
+
+};
 
 displayProductos(productos)
+
+
 const botonDeFiltro = document.querySelector("button");
 
-botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
-  }
+botonDeFiltro.onclick = function () {
 
-  const texto = $i.value;
+  const texto = filtro.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
-    
-    var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
-  }
+  const productosFiltrados = filtrado(productos, texto);
+  displayProductos(productosFiltrados);
 }
-
+//agregué toLowerCase para que no afecte si se ponen mayúsculas
 const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+  return productos.filter(item =>
+    item.tipo.toLowerCase().includes(texto.toLowerCase()) ||
+    item.color.toLowerCase().includes(texto.toLowerCase())
+  );
+
 }  
